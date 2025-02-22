82e22fecca5c3679612b8e7ce74adaab93fba6f8
DaynaKivell <dayna.kivell@gmail.com>
2025-02-22 12:25:15 +0000

    Larger window, camera rotated. And closes correctly

--- camera_test_usb.py
+++ camera_test_usb.py
@@ -1,5 +1,8 @@
 import cv2
 
+# Release the camera and destroy any open windows if they exist
+cv2.destroyAllWindows()
+
 # Use /dev/video0 (your main camera node)
 cap = cv2.VideoCapture(0)
 
@@ -25,12 +28,14 @@ while True:
         print("Failed to grab frame")
         break
 
-        # Rotate the frame 180 degrees
-    frame = cv2.rotate(frame, cv2.ROTATE_180)
 
     cv2.imshow('USB Camera', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Check if the window is closed
+    if cv2.getWindowProperty('USB Camera', cv2.WND_PROP_VISIBLE) < 1:
         break
 
 cap.release()

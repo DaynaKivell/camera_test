e9331bb321c15d4b456693faece9282753189b52
DaynaKivell <dayna.kivell@gmail.com>
2025-02-22 12:26:44 +0000

    Update camera_test_usb.py

--- camera_test_usb.py
+++ camera_test_usb.py
@@ -1,8 +1,5 @@
 import cv2
 
-# Release the camera and destroy any open windows if they exist
-cv2.destroyAllWindows()
-
 # Use /dev/video0 (your main camera node)
 cap = cv2.VideoCapture(0)
 
@@ -16,26 +13,18 @@ if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
-# Create a named window
-cv2.namedWindow('USB Camera', cv2.WINDOW_NORMAL)
-
-# Resize the window
-cv2.resizeWindow('USB Camera', 1280, 960)  # Set the desired window size
-
 while True:
     ret, frame = cap.read()
     if not ret:
         print("Failed to grab frame")
         break
 
+    # Rotate the frame 180 degrees
+    frame = cv2.rotate(frame, cv2.ROTATE_180)
 
     cv2.imshow('USB Camera', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    # Check if the window is closed
-    if cv2.getWindowProperty('USB Camera', cv2.WND_PROP_VISIBLE) < 1:
         break
 
 cap.release()
